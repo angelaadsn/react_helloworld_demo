de252a1b0b7a36f405501b7077b45b847808bb38
angelaadsn <camille.adison06@gmail.com>
2024-11-13 10:37:34 +0800

    hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [position, setPosition] = useState('top');
+
+  const handleClick = () => {
+
+    const randomTop = Math.floor(Math.random() * (window.innerHeight - 100));
+    const randomLeft = Math.floor(Math.random() * (window.innerWidth - 100));
+
+    setPosition({ top: randomTop, left: randomLeft });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={handleClick}
+
+        style={{
+          position: 'absolute',
+          top: `${position.top}px`,
+          left: `${position.left}px`,
+        }}
+      >
+        Hello World
+      </button>
     </div>
   );
-}
+};
 
 export default App;
